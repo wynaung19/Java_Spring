d9856ceef201826d341e797c80e9454e8b07fa3e
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-12-03 10:47:44 +0900

    12-3-end

--- src/main/java/com/waiyannaung/sku/config/SecurityConfig.java
+++ src/main/java/com/waiyannaung/sku/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+// import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration // 스프링 설정 클래스 지정, 등록된 Bean 생성 시점
 @EnableWebSecurity // 스프링 보안 활성화
@@ -20,7 +20,8 @@ public class SecurityConfig { // 스프링에서 보안 관리 클래스
                         .addHeaderWriter((request, response) -> {
                             response.setHeader("X-XSS-Protection", "1; mode=block"); // XSS-Protection 헤더 설정
                         }))
-                .csrf(withDefaults()) // 에러 시 import 추가
+                // .csrf(withDefaults()) // 에러 시 import 추가
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
                         .invalidSessionUrl("/session-expired") // 세션 만료시 이동 페이지
                         .maximumSessions(1) // 사용자 별 세션 최대 수
